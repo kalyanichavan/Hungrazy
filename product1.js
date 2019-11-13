4ee1a658741fd0ac11cec04b0cb81ecfa1283882
Kalyani Chavan <kp40chavan.com>
2019-11-13 16:01:05 +0530

    kkkkkjhbgjhglhjhhj

--- product1.js
+++ product1.js
@@ -3,9 +3,3 @@ var mysql=require("mysql");
 var app=express();
 var mysql=require("mysql");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
